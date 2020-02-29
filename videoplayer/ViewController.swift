e9cd91a45f956c595e5bf264f86278850eecd77a
aslowle1 <slowley.andros@gmail.com>
2020-02-29 14:28:57 -0800

    Initial Commit

---
+++ videoplayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  videoplayer
+//
+//  Created by Andros Slowley on 2/29/20.
+//  Copyright © 2020 Slowley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
